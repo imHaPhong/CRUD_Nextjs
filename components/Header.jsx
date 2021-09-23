63ca5f2e8acc6a6f538725f7472c6736a47fd707
unknown <iamhaphong15091999@gmail.com>
2021-09-23 17:09:32 +0700

    add edit form

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,25 +1,30 @@
-import styles from '../styles/Header.module.css'
-import Link from 'next/link'
-import Search from './Search'
+import styles from "../styles/Header.module.css";
+import Link from "next/link";
+import Search from "./Search";
 
 export default function Header() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                <Link href="/">
-                    <a href="">DJ Events</a>
-                </Link>
-            </div>
-            <Search/>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/events">
-                            <a href="">Events</a>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <a href="">DJ Events</a>
+        </Link>
+      </div>
+      <Search />
+      <nav>
+        <ul>
+          <li>
+            <Link href="/events">
+              <a href="">Events</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/events/add">
+              <a href="">Add Event</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
